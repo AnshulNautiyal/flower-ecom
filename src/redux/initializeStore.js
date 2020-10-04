ededf8252b42633df04c514b83e831f5f297cf9d
AnshulJS <anshul.nautiyal@ril.com>
2020-10-04 19:57:57 +0530

    add loader and toast reducer to store

--- src/redux/initializeStore.js
+++ src/redux/initializeStore.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { plpReducer } from "../pages/plp/plp-reducer";
 import { modalReducer } from "../components/Common/Modal/modal-reducer";
+import { LoaderReducer } from "../components/Common/Loader/Loader-reducer";
+import { toastReducer } from "../components/Common/Toast/Toast-reducer";
 import thunk from "redux-thunk";
 
 export default function configureStore(initialState = {}) {
   const rootReducer = combineReducers({
     plpData: plpReducer,
     modalState: modalReducer,
+    loaderState: LoaderReducer,
+    toastState: toastReducer,
   });
   const store = createStore(
     rootReducer,
@@ -14,8 +18,8 @@ export default function configureStore(initialState = {}) {
     compose(
       applyMiddleware(thunk),
       window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : f => f
+        ? window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f
     )
   );
   return store;
