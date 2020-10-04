a867d6151617a88209e739df448f6f829970da8e
AnshulJS <anshul.nautiyal@ril.com>
2020-10-04 19:54:48 +0530

    plp api changes

--- src/pages/plp/plpApiCall.js
+++ src/pages/plp/plpApiCall.js
@@ -1,6 +1,16 @@
 import axios from "axios";
 import api from "../../api/Plp-api";
-import { getPlpProductAction } from "./plp-action";
+import {
+  getPlpProductAction,
+  plpFilterData,
+  plpFetchDataFail,
+} from "./plp-action";
+import { hideLoader } from "../../components/Common/Loader/Loader-action";
+import { hideModal } from "../../components/Common/Modal/modal-action";
+import {
+  hideToast,
+  showToast,
+} from "../../components/Common/Toast/Toast-action";
 
 function getPlpData({
   category = "",
@@ -8,7 +18,7 @@ function getPlpData({
   color_id = "",
   page_no = "",
 }) {
-  const endPoint = api["getProductListInPlp"];
+  const endPoint = api.getProductListInPlp;
   const filterData = {
     category,
     price_range,
@@ -17,15 +27,31 @@ function getPlpData({
   };
 
   return async (dispatch) => {
-    const response = await axios.post(
-      endPoint,
-      filterData
-    );
-    const { data : { data = [], reponse_status = false } = {} } = response;
+    const response = await axios.post(endPoint, filterData);
+    const { data: { data = [], reponse_status = false } = {} } = response;
+    dispatch(hideLoader(false));
     if (reponse_status) {
       dispatch(getPlpProductAction(data));
+      dispatch(hideModal(false));
+    } else {
+      const { data: { errorMessage = "", subMessage = "" } = {} } = response;
+      dispatch(showToast());
+      dispatch(plpFetchDataFail(`${errorMessage}.`));
+      setTimeout(() => dispatch(hideToast()), 5000);
     }
   };
 }
 
-export default getPlpData;
+function getFilterData() {
+  const filterApiEndpoint = api.getFilterData;
+  return async (dispatch) => {
+    const filterDataResponse = await axios.get(filterApiEndpoint);
+    dispatch(hideLoader(false));
+    const {
+      data: { data = [], reponse_status = false } = {},
+    } = filterDataResponse;
+    reponse_status && dispatch(plpFilterData(data));
+  };
+}
+
+export { getPlpData, getFilterData };
