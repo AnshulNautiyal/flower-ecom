58555848e0ac27a8879a1298077289dd807a4575
AnshulJS <anshul.nautiyal@ril.com>
2020-10-04 19:49:55 +0530

    save filter api in config

--- src/api/Plp-api.js
+++ src/api/Plp-api.js
@@ -1,5 +1,6 @@
 export default {
 
-    "getProductListInPlp": "https://dreamnightsoftwares.com/flower/apis/get_product_list.php",
+    getProductListInPlp: "https://dreamnightsoftwares.com/flower/apis/get_product_list.php",
+    getFilterData: "https://dreamnightsoftwares.com/flower/apis/get_plp_filters.php"
     
 }
