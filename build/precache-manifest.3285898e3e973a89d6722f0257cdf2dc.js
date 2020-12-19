abe0933e12dd0577387f3775fb9884fc1fc9a95e
AnshulJS <anshul.nautiyal@ril.com>
2020-12-19 19:14:55 +0530

    Add build to commit for deploy

---
+++ build/precache-manifest.3285898e3e973a89d6722f0257cdf2dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "291497bd2d1fedba97b06cb7d3448d81",
+    "url": "/flower/client/index.html"
+  },
+  {
+    "revision": "b50874a74daacb8e6303",
+    "url": "/flower/client/static/css/main.fc1b3c5b.chunk.css"
+  },
+  {
+    "revision": "2ddb959afc2a6be2236d",
+    "url": "/flower/client/static/js/2.b9357c7b.chunk.js"
+  },
+  {
+    "revision": "177f179dbee68262dea2e12f4e434276",
+    "url": "/flower/client/static/js/2.b9357c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50874a74daacb8e6303",
+    "url": "/flower/client/static/js/main.f9cefdfa.chunk.js"
+  },
+  {
+    "revision": "b901e125b4ab36a6df94",
+    "url": "/flower/client/static/js/runtime-main.5c81a4e2.js"
+  },
+  {
+    "revision": "fa65e84b48c46bd98873dacfc38479a2",
+    "url": "/flower/client/static/media/bag.fa65e84b.svg"
+  },
+  {
+    "revision": "716126a174cb9dff4aafcc2db0b8781e",
+    "url": "/flower/client/static/media/desktop404.716126a1.png"
+  },
+  {
+    "revision": "12609400b7619a31ef31e98515f18441",
+    "url": "/flower/client/static/media/icomoon.12609400.ttf"
+  },
+  {
+    "revision": "29ed3edf5ccc16b4e14b0f2a0002a190",
+    "url": "/flower/client/static/media/icomoon.29ed3edf.woff"
+  },
+  {
+    "revision": "34dd0ecf14d53c44f1a0baf3965d5ba8",
+    "url": "/flower/client/static/media/logo.34dd0ecf.jpg"
+  },
+  {
+    "revision": "2b8464dc1c23120143052116e1ddc3ba",
+    "url": "/flower/client/static/media/mobile404.2b8464dc.jpg"
+  }
+]);
