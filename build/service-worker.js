e0c6f623eba942f37827d4a84dd61ff6bd4c5d1e
AnshulJS <anshul.nautiyal@ril.com>
2021-01-24 21:04:30 +0530

    remove build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13ca4450981edd74a8af037b02447684.js"
+  "/precache-manifest.180da95605b3d89039d7ea8658260b97.js"
 );
 
 self.addEventListener('message', (event) => {
