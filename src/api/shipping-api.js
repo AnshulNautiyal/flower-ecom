6558c14d00ae9cb0bee05a4e53f8df78b304755f
AnshulJS <anshul.nautiyal@ril.com>
2021-01-24 19:46:40 +0530

    shipping API

---
+++ src/api/shipping-api.js
@@ -0,0 +1,11 @@
+export default {
+  pincodeAddressApi:
+    "https://dreamnightsoftwares.com/flower/apis/get_details_by_pincode.php",
+  addAddressApi: "https://dreamnightsoftwares.com/flower/apis/add_address.php",
+  addressListApi:
+    "https://dreamnightsoftwares.com/flower/apis/get_address_list.php",
+  shippingAddressApi:
+    "https://dreamnightsoftwares.com/flower/apis/get_default_address.php",
+  editAddressApi:
+    "https://dreamnightsoftwares.com/flower/apis/update_address.php",
+};
