e43ed85f73bb7dceecd814148dc37cbd334d38f3
AnshulJS <anshul.nautiyal@ril.com>
2021-01-24 19:47:35 +0530

    Add route for shipping

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -6,7 +6,7 @@ import Home from "./../pages/home";
 import Plp from "./../pages/plp";
 import Pdp from "./../pages/pdp";
 import Cart from "./../pages/Cart";
-// import Shipping from "./../pages/shipping";
+import Shipping from "./../pages/shipping";
 import {OrderConfirmationComponent} from "./../pages/orderConfirmation/index";
 import { Page404 } from "./../pages/404page";
 import { BrowserComponent } from "./../components/BrowserComponent";
@@ -27,7 +27,7 @@ export const Routes = () => {
           <Route exact path="/c/:id" component={Plp} />
           <Route exact path="/pdp/:name/:id" component={Pdp} />
           <Route exact path="/cart" component={Cart} />
-          {/* <Route exact path="/shipping" component={Shipping} /> */}
+          <Route exact path="/shipping" component={Shipping} />
           <Route exact path="/order-confirm" component={OrderConfirmationComponent} />
           <Route component={Page404} />
         </Switch>
